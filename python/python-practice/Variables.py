2d98f235e18ab7d8aca5c9b15b85a132467f40d2
Harish <harish.jadhav60@gmail.com>
2026-05-11 10:16:04 +0530

    Variable

--- python/python-practice/Variables.py
+++ python/python-practice/Variables.py
@@ -2,6 +2,13 @@
 name ="Harish"
 age = 23
 price = 100.50
+double = "She said, \"That's a great testing apple!\""
+string_length = len(double)
+
 
 print("My Name is : ", name)
 print("My age is : ", age)
+print(double)
+print(string_length)
+print(len(double))
+print(len("apple"))
