49f8eef16476278f7ff4083aa34f820814e4e6de
Harish <harish.jadhav60@gmail.com>
2026-05-11 11:18:20 +0530

    String Methods

---
+++ python/python-practice/string-methods.py
@@ -0,0 +1,15 @@
+fruit = "Apple"
+
+print(len(fruit))
+
+print(type(fruit))
+
+print(fruit.lower())
+print(fruit.upper())
+
+#Repeating Strings
+
+print('*' * 10)
+
+print('I {} Python.'.format('Love'))
+print('{} {} {}'.format('I', 'Love', 'Python'))
