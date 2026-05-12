45220d1abeb28c99e45d217ac60e72a729784b75
Harish <harish.jadhav60@gmail.com>
2026-05-12 10:01:12 +0530

    Boolean and Conditional Statements

---
+++ python/python-practice/conditional-and-boolean.py
@@ -0,0 +1,10 @@
+
+a_boolean = True
+print(a_boolean)
+
+a = 10
+b = 10
+if a > b:
+  print("a is greater than b")
+elif a < b : print ("a is less than b")
+else: print ("both equal")
--- python/python-practice/string-methods.py
+++ python/python-practice/string-methods.py
@@ -13,3 +13,10 @@ print('*' * 10)
 
 print('I {} Python.'.format('Love'))
 print('{} {} {}'.format('I', 'Love', 'Python'))
+
+"""This 
+is 
+the 
+multiline
+comment
+"""
