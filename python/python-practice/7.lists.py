ba98f62357c37fd3baae52f632737ab79ef5bf02
Harish <harish.jadhav60@gmail.com>
2026-05-13 09:15:07 +0530

    Lists

---
+++ python/python-practice/7.lists.py
@@ -0,0 +1,28 @@
+
+animals = ['Tiger', 'bear', 'elephant']
+
+# print(animals[0])
+# print(animals[1])
+# print(animals[2])
+
+animals[0] = 'cat'
+
+# print(animals[0])
+
+# print(animals[-1])
+
+# print(animals[len(animals)-1])
+
+animals.append('cow')
+
+# print(animals[len(animals)-1])
+
+# print(animals)
+
+animals.extend(['duck', 'horse'])
+
+print(animals)
+
+animals.insert(2, 'pig')
+
+print(animals)
